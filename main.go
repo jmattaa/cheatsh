06b9521b839d270d9dbde5f7c14b921982defa28
jmattaa <mattajonathan1@gmail.com>
2025-02-10 08:21:14 +0100

    output write to file

--- main.go
+++ main.go
@@ -3,14 +3,16 @@ package main
 import (
 	"flag"
 	"log"
+	"os"
 
 	"github.com/jmattaa/cheatsh/ai"
 	"github.com/jmattaa/cheatsh/formatter"
 )
 
 func main() {
-	var topic string
+	var topic, outputfile string
 	flag.StringVar(&topic, "topic", "", "get a cheat sheet on this topic")
+	flag.StringVar(&outputfile, "o", "", "write to this file")
 	flag.Parse()
 
 	if topic == "" {
@@ -20,8 +22,16 @@ func main() {
 
 	println("getting a cheat sheet on:", topic)
 
-    res := ai.GetCheatSheet(topic)
+	res := ai.GetCheatSheet(topic)
+	formatter.Print(res)
 
-    formatter.Print(res)
+	// write to file
+    if outputfile == "" {
+        return
+    }
+
+    err := os.WriteFile(outputfile, []byte(res), 0644)
+    if err != nil {
+        log.Fatal(err)
+    }
 }
-
